6f89a19925618cac03572175b5ad145e1765f94f
sohelrana22 <sohel252646@gmail.com>
2021-10-31 23:58:28 +0600

    finish

--- src/pages/ManageOrder/ManageOrder.js
+++ src/pages/ManageOrder/ManageOrder.js
@@ -1,11 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import useFirebase from '../../hooks/useFirebase';
 
 const ManageOrder = () => {
+    const { user } = useFirebase()
+    const { email } = user;
+    const [allOrder, setAllOrder] = useState([])
+    console.log(email);
+    useEffect(() => {
+        fetch(`https://pure-oasis-89379.herokuapp.com/manageorder/${email}`)
+            .then(res => res.json())
+            .then(data => setAllOrder(data));
+    }, [email])
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure, You want to cancel Order?');
+        if(proceed) {
+          const url = `https://pure-oasis-89379.herokuapp.com/manageorder/${id}`;
+          fetch(url, {
+              method: 'DELETE'
+          })
+              .then(res => res.json())
+              .then(data => {
+                  console.log(data);
+                  if (data.deletedCount) {
+                      alert('canceled')
+                      const remaining = allOrder.filter(single => single?._id !== id);
+                      setAllOrder(remaining)
+                  }
+  
+              })
+        }
+      }
     return (
-        <div>
-            <h2>manageorder</h2>
+        <div className="admin-section p-5">
+          <div className='text-center fw-bold'>
+            <h1 className='pt-3'>ADMIN DASHBOARD</h1>
+             <h4 className='pt-2 pb-2'>TOTAL ORDERS:  { allOrder.length}</h4>
+          </div>
+          <div className='container'>
+             <Table striped bordered hover
+     responsive="sm" className='my-3'>
+        <thead>
+          <tr>
+                        {Array.from({ length: 1 }).map((_, index) => (
+                                    <th key={index}>Name</th>
+                        ))}
+                
+                        {Array.from({ length: 1 }).map((_, index) => (
+                                    <th key={index}> ORDER ID </th>
+                        ))}
+                
+    
+                        {Array.from({ length: 1 }).map((_, index) => (
+                          <th key={index}>PRICE</th>
+                        ))}
+                
+                        {Array.from({ length: 1 }).map((_, index) => (
+                                    <th key={index}>STATUS</th>
+                        ))}
+                        {Array.from({ length: 1 }).map((_, index) => (
+                                    <th key={index}>CANCEL</th>
+                        ))}
+                        {Array.from({ length: 1 }).map((_, index) => (
+                                    <th key={index}>CONFIRM</th>
+                        ))}
+              </tr>
+              
+            </thead>
+            {
+              allOrder.map(allOrder=>(<tbody>
+        
+        <tr>
+          
+          {Array.from({ length: 1 }).map((_, index) => (
+            <td className='fw-bold ' key={index}>{ allOrder?.name}</td>
+          ))}
+                  
+          {Array.from({ length: 1 }).map((_, index) => (
+            <td className='fw-bold ' key={index}>{ allOrder?._id}</td>
+          ))}
+                  
+          {Array.from({ length: 1 }).map((_, index) => (
+            <td className='fw-bold ' key={index}>{ allOrder?.price}</td>
+          ))}
+                  
+          {Array.from({ length: 1 }).map((_, index) => (
+            <td className='fw-bold ' key={index}>{ allOrder?.status}</td>
+          ))}
+          {Array.from({ length: 1 }).map((_, index) => (
+            <td  key={index}>Panding<button onClick={() => handleDelete(allOrder._id)} className="mx-3 text-danger">Delete</button></td>
+          ))}
+          {Array.from({ length: 1 }).map((_, index) => (
+            <td  key={index}></td>
+          ))}
+                  
+                  
+    
+                  
+        </tr>
+        
+    </tbody>))
+            }
+        </Table>
+       </div>
+    
         </div>
-    );
-};
+      );
+    };
 
 export default ManageOrder;
--- src/pages/MyOrder/MyOrder.js
+++ src/pages/MyOrder/MyOrder.js
@@ -1,36 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { Col, Container, Row } from 'react-bootstrap';
 import useFirebase from '../../hooks/useFirebase';
+import './MyOrder.css';
 
 const MyOrder = () => {
-   const {user} = useFirebase()
-   const {email} = user;
+    const { user } = useFirebase()
+    const { email } = user;
     const [order, setOrder] = useState([])
-console.log(email);
-    useEffect( () => {
+    console.log(email);
+    useEffect(() => {
         fetch(`https://pure-oasis-89379.herokuapp.com/orders/${email}`)
-        .then(res => res.json())
-        .then(data => setOrder(data));
+            .then(res => res.json())
+            .then(data => setOrder(data));
     }, [email])
-    // console.log(order);
+    const handleDelete = email => {
+      const proceed = window.confirm('Are you sure, You want to cancel Order?');
+      if(proceed) {
+        const url = `https://pure-oasis-89379.herokuapp.com/orders/${email}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    alert('canceled')
+                    const remaining = order.filter(single => single?.email !== email);
+                    setOrder(remaining)
+                }
+
+            })
+      }
+    }
     return (
         <div>
-            <h2 className="mx-auto">My Order:{order.length}</h2>
-           <div>
-        {
-            order.map(single=><div className="container d-flex">
-                <div>
-                    <img className="w-50" src={single.img} alt="" />
-                    
-                 <div>
-               <h2>{single.name}</h2>
-                <h4>{single.price}</h4>
-                <p>{single.detail}</p>
-               </div>
-                </div>
-              
-            </div>)
-        }
+            <h2 className="order">My Order:{order.length}</h2>
+            <div>
+                {
+                    order.map(single => <div className="my-4" key={single._id}>
+                        <Container className="bg-info">
+                            <Row>
+                                <Col>
+                                    <img className="img-fluid" src={single.img} alt="" />
+                                </Col>
+                                <Col>
+                                    <h1>{single.name}</h1>
+                                    <h2>Price: ${single.price}</h2>
+                                    <h3>{single.detail}</h3>
+                                    <button className="px-3 bg-danger" onClick={() => handleDelete(single.email)}>Cancel Order</button>
+                                </Col>
+                            </Row>
+                        </Container>
+
+                    </div>)
+
+                }
             </div>
         </div>
     );
